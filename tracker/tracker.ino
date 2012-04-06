724c35f0184195e74e2766ccbcbc6931f2bf5901
Alex Hutton <ajhutton@gmail.com>
2012-04-06 12:50:47 +0930

    tidied up the code. added settings and proper debug mode config.

--- tracker/tracker.ino
+++ tracker/tracker.ino
@@ -1,57 +1,67 @@
 /*TinyTracker 0.1
 High-Altitude Balloon Payload Code
-Last updated 15/03/2012
 Released under the GNU/GPL License
 by Alex Hutton*/
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "TinyGPS.h"
-//http://ukhas.org.uk/guides:falcom_fsa03 modified tinygps?
 #include "RTTY.h"
-//Let's set up the objects
-//Mark_Pin, Space_Pin, ASCII (7 or 8), Num Stop Bits (1 or 2), Baud (50 or 300)
-RTTY rtty(4,5,7,1,50);
-// rx, tx pins
-SoftwareSerial debugSerial(8, 9);
-TinyGPS gps;
-//name progress/new skills
-// our sprintf buffer 
+//CONFIG
+//RTTY CONFIG
+int    MARK     = 4; //RTTY MARK pin
+int    SPACE    = 5; //RTTY SPACE pin
+int    ASCII    = 7; //ASCII = 7 or 8
+int    STOPBITS = 1; //STOPBITS = 1 or 2
+int    BAUD     = 50;//BAUD = 50 or 300
+//DEBUG CONFIG
+int    debug    = 1; //debugging to software serial config (0=off, 1=only print rtty sentences, 2=echo GPS data as well)
+int    debugRX  = 8; //DEBUG software serial RX pin
+int    debugTX  = 9; //DEBUG software serial TX pin
+int    dbgBaud  = 9600;//DEBUG software serial baud rate
+//MISC CONFIG
+String callsign = "HUTTON"; //balloon callsign for rtty transmission
+int    checksum = 0; //produce checksum for RTTY sentence (0=off, 1=on)
+int    gpsBaud  = 9600; //baudrate of hardware serial port for GPS
+//END CONFIG
+
+//GPS Data Variables
 char superbuffer[100]=""; 
-//char callsign[]="HUTTON";
-//we need some vars to hold GPS data in
 unsigned long fix_age, time, date, speed, course, altitude;
 unsigned long chars;
 unsigned short sentences, failed_checksum;
-int debug = 1;
 
-//required arduino "setup" routine. this is run on poweron immediately.
+//Setup objects
+RTTY rtty(MARK,SPACE,ASCII,STOPBITS,BAUD);
+TinyGPS gps;
+SoftwareSerial debugSerial(debugRX, debugTX);
+
 void setup() {
-Serial.begin(9600);
-if(debug = 1) debugSerial.begin(9600);
-pinMode(2, OUTPUT);
+Serial.begin(gpsBaud);
+if(debug > 0) {
+  debugSerial.begin(9600);
+  debugSerial.print("Level ");
+  debugSerial.print(debug);
+  debugSerial.println(" debugging enabled. Logging to serial");
+}
 }
 
 //required arduino "loop" routine. this is run after the setup is complete. it loops.
 void loop() {
   while (Serial.available())
   {
-    digitalWrite(2, HIGH);
     int c = Serial.read();
-    if (debug = 1) debugSerial.write(c);
+    if (debug >= 2) debugSerial.write(c);
     if (gps.encode(c))
     {
-//process data
+      if(debug > 0) debugSerial.println("Got fix! Parsing...");
       long lat, lon;
       gps.get_position(&lat, &lon, &fix_age);
       gps.get_datetime(&date, &time, &fix_age);
       speed = gps.speed();
       altitude = gps.altitude(); //fix this
       course = gps.course();
-//printf works like printf ("%d, %d, %d, %d, %d", 1Value, 2Value, , ,);
-//$$<CALL SIGN>,<COUNTER D>,<TIME HH:MM:SS>,<LATITUDE DD.DDDDDD>,<LONGITUDE DD.DDDDDD>,<ALTITUDE METRES MMMMM>*<CHECKSUM><NEWLINE>
-char sent = sprintf(superbuffer,"$$HUTTON,%d,%d,%d,%d,%d",sentences,time,lat,lon,altitude);
-
-if (debug = 1) debugSerial.println(); debugSerial.println(superbuffer);
+      char sent = sprintf(superbuffer,"$$%d,%d,%d,%d,%d,%d*%d\n",callsign[1],sentences,time,lat,lon,altitude,checksum);
+      if (debug >= 1) debugSerial.println(); debugSerial.println(superbuffer);
     }
   }
 
