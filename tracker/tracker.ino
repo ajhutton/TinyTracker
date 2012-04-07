a6cef2c46e20f48c61ec7c4d273f1eaeac5e73c9
Alex Hutton <ajhutton@gmail.com>
2012-04-07 23:33:47 +0930

    changed settings from int to const int

--- tracker/tracker.ino
+++ tracker/tracker.ino
@@ -1,5 +1,5 @@
 /*
-  TinyTracker
+ TinyTracker
  High-Altitude Balloon Payload Code
  Copyright 2012 Alex Hutton
  
@@ -21,19 +21,19 @@
 #include "TinyGPS.h"
 //CONFIG
 //RTTY CONFIG
-int PIN_RTTY_SPACE  = 2;
-int PIN_RTTY_MARK   = 3;
-int PIN_RTTY_ENABLE = 4; 	// Active High
+const int PIN_RTTY_SPACE  = 2;
+const int PIN_RTTY_MARK   = 3;
+const int PIN_RTTY_ENABLE = 4; 	// Active High
 //DEBUG CONFIG
-int debug           = 1; //debugging to software serial config (0=off, 1=only print rtty sentences, 2=echo GPS data as well)
-int debugRX         = 8; //DEBUG software serial RX pin
-int debugTX         = 9; //DEBUG software serial TX pin
-int dbgBaud         = 9600;//DEBUG software serial baud rate
+const int debug           = 1; //debugging to software serial config (0=off, 1=only print rtty sentences, 2=echo GPS data as well)
+const int debugRX         = 8; //DEBUG software serial RX pin
+const int debugTX         = 9; //DEBUG software serial TX pin
+const int dbgBaud         = 9600;//DEBUG software serial baud rate
 //MISC CONFIG
-int checksum        = 0; //produce checksum for RTTY sentence (0=off, 1=on) NOT YET IMPLEMENTED
-int gpsBaud         = 9600; //baudrate of hardware serial port for GPS
-int bitRate         = 50;
-int PIN_PWR_LED     = 13;
+const int checksum        = 0; //produce checksum for RTTY sentence (0=off, 1=on) NOT YET IMPLEMENTED
+const int gpsBaud         = 9600; //baudrate of hardware serial port for GPS
+const int bitRate         = 50;
+const int PIN_PWR_LED     = 13;
 //END CONFIG
 
 //Setup objects
@@ -88,6 +88,7 @@ void loop() {
   getGPS();
   genSentence();
   debugPrint(txBuffer);
+  txString(txBuffer);
   delay(1000);
 }
 
@@ -96,24 +97,21 @@ void genSentence() {
     dtostrf(record1.latitude, 11, 5, latString);
     dtostrf(record1.longitude, 11, 5, longString);
   }
+  
   sprintf(txBuffer, "$$HUTTON,%u,%02u:%02u:%02d,%s,%s,%u,%u,%u,%d,%d", record1.sequence_no, record1.hour, record1.minute, record1.second, trim(latString), trim(longString), record1.altitude, record1.velocity, record1.sats, record1.temp1, record1.temp2);
   record1.sequence_no++;
 }
 
-int debugPrint(String msg) {
+void debugPrint(String msg) {
   if(debug != 0) {
     //         sprintf(debugBuffer, "[%u]%s", millis(), msg); 
-    debugSerial.print("[");
-    debugSerial.print(millis());
-    debugSerial.print("]");
+   // debugSerial.print("[");
+   // debugSerial.print(millis());
+  //  debugSerial.print("]");
     debugSerial.println(msg);
 
     //          debugSerial.println(debugBuffer);
-    return 1;
 
-  }
-  else {
-    return 0;
   }
 }
 
